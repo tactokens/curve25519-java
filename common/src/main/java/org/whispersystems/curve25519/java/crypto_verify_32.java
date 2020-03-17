c2911bda51dec3414fd818c6ddc095d0060bea52
Sergey Tolmachev <tolsi.ru@gmail.com>
2020-03-17 12:29:11 +0300

    crypto verify comment was added

--- common/src/main/java/org/whispersystems/curve25519/java/crypto_verify_32.java
+++ common/src/main/java/org/whispersystems/curve25519/java/crypto_verify_32.java
@@ -2,6 +2,12 @@ package org.whispersystems.curve25519.java;
 
 public class crypto_verify_32 {
 
+    /**
+     * const time equality check of x and y byte arrays of length 32
+     * @param x first byte array
+     * @param y second byte array
+     * @return 0 if equals, other value if not
+     */
     public static int crypto_verify_32(byte[] x, byte[] y) {
         int differentbits = 0;
         for (int count = 0; count < 32; count++) {
