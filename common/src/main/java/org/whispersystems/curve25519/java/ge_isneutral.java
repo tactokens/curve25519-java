9c66816f62ae446de9f39d3c4c28062656c64e38
Sergey Tolmachev <tolsi.ru@gmail.com>
2020-03-17 12:31:19 +0300

    ge_isneutral was fixed

--- common/src/main/java/org/whispersystems/curve25519/java/ge_isneutral.java
+++ common/src/main/java/org/whispersystems/curve25519/java/ge_isneutral.java
@@ -1,11 +1,11 @@
 package org.whispersystems.curve25519.java;
 
 public class ge_isneutral {
-    /*
-    return 1 if p is the neutral point
-    return 0 otherwise
-    */
-
+    /**
+     * Checks that the point is neutral
+     * @param p point
+     * @return true if p is the neutral point, false otherwise
+     */
     public static boolean ge_isneutral(ge_p3 p)
     {
         int[] zero = new int[10];
